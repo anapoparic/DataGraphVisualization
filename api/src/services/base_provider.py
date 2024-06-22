1acd1757df0a53e349ea461dd527e5aaed667a9b
Vesna Vasic <vasic.vesna.06@gmail.com>
2024-06-22 19:30:21 +0200

    #5.2 Added SourcePlugin abstract class with methods

---
+++ api/src/services/base_provider.py
@@ -0,0 +1,12 @@
+from abc import abstractmethod
+from api.src.services.base_service import ServiceBase
+from api.src.types.config import Config
+
+class SourcePlugin(ServiceBase):
+    @abstractmethod
+    def load(self, config: dict):
+        pass
+
+    @abstractmethod
+    def config(self) -> list[Config]:
+        pass
