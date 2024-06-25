a0ec8bee49c38ffef248e8ef2218580b3da797d8
anapoparic <anapoparic28@gmail.com>
2024-06-25 23:17:54 +0200

    #25.1 Block and simple visualizer successfully refactored and completed

--- graph_visualization/app/views.py
+++ graph_visualization/app/views.py
@@ -3,10 +3,10 @@ import sys
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import ensure_csrf_cookie
 
 from django.views.decorators.csrf import csrf_exempt
 
+from core.src.use_cases import tree_view
 from core.src.use_cases.loader import Loader
 from core.src.use_cases.tree_view import TreeView
 
@@ -48,16 +48,19 @@ def clear_filters(request):
 def set_workspace(request, number: int):
     pass
 
+
 def render_new_graph(request):
     global workspace_id, source_id, visualizer_id
     visualizer_id = int(request.POST.get("visualizers"))
     source_id = int(request.POST.get("sources"))
 
     graph = loader.get_loaded_graph(source_id)
+    tree_view = TreeView(graph)
     visualization_html = loader.visualizers[visualizer_id].plugin.visualize(graph)
 
     return render(request, 'index.html', {
         "sources": loader.sources,
         "visualizers": loader.visualizers,
-        "visualization_html": visualization_html
+        "visualization_html": visualization_html,
+        "tree_view_html": tree_view.generate_tree_view()
     })
